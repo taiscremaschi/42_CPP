18d9f0311befe022997bf5284fb18fbd474d7f95
taiscremaschi <taiscremaschi@gmail.com>
2024-03-20 14:51:50 +0000

    nao copila

--- CPP_00/ex01/main.cpp
+++ CPP_00/ex01/main.cpp
@@ -2,39 +2,93 @@
 #include <string>
 
 class Contact {
-std::string contact_nbr;
-std::string first_name;
-std::string last_name;
-std::string nickname;
-std::string darkest_secret;
+private:
+    std::string _contact_nbr;
+    std::string _first_name;
+    std::string _last_name;
+    std::string _nickname;
+    std::string _darkest_secret;
+public:
+    Contact();
+    Contact(std::string contact_nbr, std::string first_name, std::string last_name,
+        std::string nickname, std::string darkest_secret);
+    ~Contact() {};
+
 };
 
 class Phone_Book {
-
-int index;
-Contact contacts[8];
+private:
+    int index;
+    Contact contacts[7];
+public:
+    Phone_Book() : index(0) {}
+    void add_contact(const Contact &new_contact);
 
 };
 
+void Phone_Book::add_contact(const Contact &new_contact) 
+{
+    if (index < 7) 
+    {
+        contacts[index++] = new_contact;
+        std::cout << "YES!! Contact added successfully!\n";
+    } 
+    else 
+         contacts[0] = new_contact;
+    std::cout << "YES!! Contact added successfully!\n";
+
+}
+
+void adc_contacts(Phone_Book &phone_book) 
+{
+    std::string first_name, last_name, contact_nbr, nickname, darkest_secret;
+
+    std::cout << "Enter the first name: ";
+    std::getline(std::cin, first_name);
+
+    std::cout << "Enter the last name: ";
+    std::getline(std::cin, last_name);
+
+    std::cout << "Enter the contact number: ";
+    std::getline(std::cin, contact_nbr);
+
+    std::cout << "Enter the nickname: ";
+    std::getline(std::cin, nickname);
+
+    std::cout << "Enter the darkest secret: ";
+    std::getline(std::cin, darkest_secret);
+
+    Contact new_contact(contact_nbr, first_name, last_name, nickname, darkest_secret);
+    phone_book.add_contact(new_contact);
+}
+
 int main()
 {
+    Phone_Book phone_book;
     while(1)
     {
 
         std::string input;
-        std::cout << "Enter the name: ";
+        std::cout << "Enter command ADD or SEARCH OR EXIT: ";
         std::getline(std::cin, input);
         if(input == "ADD")
         {
-
+           adc_contacts(phone_book);
         }
         if(input == "SEARCH")
         {
+            std::cout << "procurando";
 
         }
         if(input == "EXIT")
+        {
+            std::cout << "Bye!! See you next time! :) ";
             break ;
-        std::cout << "Hello " << input << "!\n";
+        }
+        else 
+            std::cout << "invalid command";
+
+
     }
     
         // se for add, abro a classe phone_book e comeco a escrever em cada lugar 
