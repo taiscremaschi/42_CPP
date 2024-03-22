34533342c26db392d6e8d5376d7b51a2de86793d
taiscremaschi <taiscremaschi@gmail.com>
2024-03-22 10:53:19 +0000

    inicializando search

--- CPP_00/ex01/main.cpp
+++ CPP_00/ex01/main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <cctype> 
 #include <limits.h>
-
+#include <iomanip> 
 
 #define RESET   "\033[0m"
 #define RED     "\033[31m"      
@@ -29,40 +29,43 @@ public:
             _nickname = nickname;
             _darkest_secret = darkest_secret;
         }
+    std::string getFistName();
+    std::string getLastName();
+    std::string getNickname();
+
     ~Contact() {};
 };
 
 class Phone_Book {
 private:
-    int index;
     Contact contacts[8];
+    int _index;
 public:
-    Phone_Book() : index(0) {}
+    Phone_Book() : _index(0) {}
     void add_contact(const Contact &new_contact);
-
+    int get_index();
+    Contact getContact(int i);
 };
+
+int     Phone_Book::get_index()
+{
+    return(_index);
+}
 
 void Phone_Book::add_contact(const Contact &new_contact) 
 {
-    static int flag;
 
-    if (index < 7) 
-    {
-        contacts[index] = new_contact;
-        index++;
-        std::cout << CYAN << "YES!! Contact added successfully!\n" << RESET;
-    }
-    else if( flag == 1 ) {
-        index = 0;
-        contacts[index] = new_contact;
-        index++;
-        std::cout << CYAN << "YES!! Contact added successfully!\n" << RESET;
-    }
-    else
-        flag = 1;
+    contacts[_index % 8] = new_contact;
+    _index++;
+    std::cout << CYAN << "YES!! Contact added successfully!\n" << RESET;
 }
 
-bool is_number(std::string &str)
+Contact Phone_Book::getContact(int i)
+{
+    return (contacts[i]);
+}
+
+bool is_not_number(std::string &str)
 {
     int i = 0;
     if(!str[i])
@@ -88,7 +91,7 @@ void adc_infos(Phone_Book &phone_book)
     {
         std::cout << MAGENTA << "Enter the contact number: " << RESET;
         std::getline(std::cin, contact_nbr);
-        if(is_number(contact_nbr) == true)
+        if(is_not_number(contact_nbr) == true)
             break ;
         std::cout << RED << "Please, insert just numbers\n" << RESET;
     }
@@ -102,9 +105,51 @@ void adc_infos(Phone_Book &phone_book)
     phone_book.add_contact(new_contact);
 }
 
+void trunc(Contact &contact, int size)
+{
+
+}
+
+void search_contact (Contact &contacts, Phone_Book &phone_book)
+{
+    int i = 0; 
+    int index = phone_book.get_index();
+
+    std::cout << std::setw(10) << std::right << "Index" << " | "
+          << std::setw(10) << std::right << "First Name" << " | "
+          << std::setw(10) << std::right << "Last Name" << " | "
+          << std::setw(10) << std::right << "Nickname" << std::endl;
+    while(i <= index)
+    {
+        std::cout << std::setw(10) << std::right << i << " | ";
+        std::cout << std::setw(10) << std::right << trunc(phone_book.getContact(i).first_name(), 10) << " | ";
+        std::cout << std::setw(10) << std::right << trunc(phone_book.getContact(i).last_name(), 10) << " | ";
+        std::cout << std::setw(10) << std::right << trunc(phone_book.getContact(i).nickname(), 10) << std::endl;
+    }
+        
+}
+void choise_contact(Phone_Book &phone_book)
+{
+
+
+}
+
+int ft_atoi(std::string s)
+{
+    int result = 0;
+    if(s[1])
+        return -1;
+    if(s[0] > '8')
+        return -1;
+   result =  result + s[0] - '0';
+    return result;
+}
+
+
 int main()
 {
     Phone_Book phone_book;
+    Contact contacts;
 
     std::cout << CYAN << "WELCOME TO THIS FANTASTIC TRIP!\n" << RESET;
     while(1)
@@ -116,15 +161,28 @@ int main()
         if(input == "ADD")
         {
            adc_infos(phone_book);
-           continue ;
         }
-        if(input == "SEARCH")
+        else if(input == "SEARCH")
         {
-            std::cout << "procurando";
-            continue ;
-
+            while(1)
+            {
+                search_contact(contacts, phone_book);
+                std::cout << CYAN << "Choose the index you need or to return press '.': " << RESET;
+                std::getline(std::cin, input);
+                if(input == ".")
+                {
+                    break ;
+                }
+                if(ft_atoi(input) <= phone_book.get_index() && ft_atoi(input) > -1)
+                {
+                    choise_contact(phone_book);
+                    continue ;
+                }
+                else 
+                    std::cout <<  RED << "invalid index, try again!\n" << RESET;
+            }
         }
-        if(input == "EXIT")
+        else if(input == "EXIT")
         {
             std::cout << CYAN << "Bye!! See you next time! :)\n " << RESET;
             break ;
