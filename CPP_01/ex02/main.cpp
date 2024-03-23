9f17d224620566beda017ca064bc65004eb45671
taiscremaschi <taiscremaschi@gmail.com>
2024-03-23 19:27:27 +0000

    ex02 done

--- CPP_01/ex02/main.cpp
+++ CPP_01/ex02/main.cpp
@@ -1,12 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tbolzan- <tbolzan-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 18:59:30 by tbolzan-          #+#    #+#             */
-/*   Updated: 2024/03/23 18:59:31 by tbolzan-         ###   ########.fr       */
+/*   Updated: 2024/03/23 19:26:36 by tbolzan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+
+int main(){
+
+    std::string string = "HI THIS IS BRAIN";
+    std::string *stringPTR = &string;
+    std::string &stringREF = string;
+ 
+    
+    std::cout << &string << std::endl;
+    std::cout << stringPTR<< std::endl;
+    std::cout << &stringREF << std::endl;
+
+       
+    std::cout << string << std::endl;
+    std::cout << *stringPTR << std::endl;
+    std::cout << stringREF << std::endl;
+}
