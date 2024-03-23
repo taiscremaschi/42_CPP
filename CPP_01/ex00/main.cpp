8774a6cdbdce1869e14dc5015a819768c0acc5e5
taiscremaschi <taiscremaschi@gmail.com>
2024-03-23 13:57:56 +0000

    exercicio 0 ok, sem refatoracao

--- CPP_01/ex00/main.cpp
+++ CPP_01/ex00/main.cpp
@@ -12,28 +12,49 @@ public:
     void announce();
 
 };
-
+//////////////////////////////////// funcoes da classe ////////////////////////////////////////////////
 Zombie::Zombie() {}
 
-Zombie::~Zombie() {}
+Zombie::~Zombie() {
+    std::cout << _name << " destroyed" << std::endl;
+}
 
 Zombie::Zombie(std::string name)
 {
     _name = name;
 }
 
+std::string Zombie::getName()
+{
+    return _name;
+}
+
 void Zombie::announce()
 {
     std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+Zombie *newZombie( std::string name)
+{
+    Zombie *zombie = new Zombie(name);
 
+   return zombie;
+
+}
+
+void randomChump( std::string name )
+{
+    Zombie zombie(name);
+    zombie.announce();
+}
 
 int main()
 {
-    Zombie tata("tata");
-
-    tata.announce();
-
+    Zombie *zombie = newZombie("tata");
+    zombie->announce();
+    delete zombie;
+    randomChump("ricardinho");
+    randomChump("dioguinho");
 
 }
