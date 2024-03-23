483e371f20c95699ca55ebaaf1a7e1ff32a8f63d
taiscremaschi <taiscremaschi@gmail.com>
2024-03-23 20:09:58 +0000

    progress

--- CPP_01/ex03/Weapon.hpp
+++ CPP_01/ex03/Weapon.hpp
@@ -6,7 +6,16 @@
 /*   By: tbolzan- <tbolzan-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 19:41:50 by tbolzan-          #+#    #+#             */
-/*   Updated: 2024/03/23 19:41:51 by tbolzan-         ###   ########.fr       */
+/*   Updated: 2024/03/23 20:05:10 by tbolzan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
+#include <iostream>
+
+class Weapon{
+  private:
+    std::string _type;
+  public:
+    getType()
+};
