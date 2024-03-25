f16613d5c4498d2c71eee4d9e3fd9075a71b2a33
taiscremaschi <taiscremaschi@gmail.com>
2024-03-25 18:21:57 +0000

    add classes

--- CPP_01/ex05/Harl.cpp
+++ CPP_01/ex05/Harl.cpp
@@ -6,10 +6,28 @@
 /*   By: tbolzan- <tbolzan-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 18:04:59 by tbolzan-          #+#    #+#             */
-/*   Updated: 2024/03/25 18:05:05 by tbolzan-         ###   ########.fr       */
+/*   Updated: 2024/03/25 18:21:25 by tbolzan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
+Harl::Harl(){}
 
+Harl::~Harl(){}
+
+void Harl::debug(){
+    std::cout << "I love having extra bacon for my 7XL-double-cheese. I really do!" << std::endl;
+}
+
+void Harl::info(){
+    std::cout << "I cannot believe adding extra bacon costs more money. If you did, I wouldn't be asking for more!" << std::endl;
+}
+void Harl::warning(){
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+
+}
+
+void Harl::error(){
+     std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
