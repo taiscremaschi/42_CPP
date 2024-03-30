b8e300bea7e126f1508b7004d5a990e93d3f7970
taiscremaschi <taiscremaschi@gmail.com>
2024-03-30 11:34:44 +0000

    start cpp_03

--- CPP_01/ex05/Harl.cpp
+++ CPP_01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: tbolzan- <tbolzan-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 18:04:59 by tbolzan-          #+#    #+#             */
-/*   Updated: 2024/03/25 18:52:38 by tbolzan-         ###   ########.fr       */
+/*   Updated: 2024/03/26 09:00:23 by tbolzan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,19 @@ Harl::Harl(){}
 Harl::~Harl(){}
 
 void Harl::debug(){
-    std::cout << "I love having extra bacon for my 7XL-double-cheese. I really do!" << std::endl;
+    std::cout << "I really wanted to make dinner when I got home today!" << std::endl;
 }
 
 void Harl::info(){
-    std::cout << "I cannot believe adding extra bacon costs more money. If you did, I wouldn't be asking for more!" << std::endl;
+    std::cout << "Who left this kitchen upside down? There are pancakes on the ceiling!" << std::endl;
 }
 void Harl::warning(){
-    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+    std::cout << "What do you mean you're going to football now and you're going to leave this kitchen like this?" << std::endl;
 
 }
 
 void Harl::error(){
-     std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+     std::cout << "We need to talk." << std::endl;
 }
 
 void Harl::complain(std::string level){
