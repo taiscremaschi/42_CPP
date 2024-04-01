c83c44ed55435fe7d38f1535eef51ef903aeb803
taiscremaschi <taiscremaschi@gmail.com>
2024-04-01 12:21:57 +0100

    done without  tests

--- CPP_03/ex01/ScavTrap.hpp
+++ CPP_03/ex01/ScavTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: tbolzan- <tbolzan-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 09:48:10 by tbolzan-          #+#    #+#             */
-/*   Updated: 2024/04/01 10:48:51 by tbolzan-         ###   ########.fr       */
+/*   Updated: 2024/04/01 12:15:33 by tbolzan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@ class ScavTrap : public ClapTrap {
 private:
 
 public:
-    ScavTrap(); // constructwor
+    ScavTrap(); // constructor
     ScavTrap(const std::string name); //constructor with parameter
     ScavTrap(const ScavTrap &other); //copy constructor
     ~ScavTrap(); // destructor
     ScavTrap &operator=(ScavTrap &other); // assignment operator
     void guardGate();
-    
+    void attack(const std::string &target);
 };
 
 
--- CPP_03/ex01/main.cpp
+++ CPP_03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tbolzan- <tbolzan-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 09:47:56 by tbolzan-          #+#    #+#             */
-/*   Updated: 2024/04/01 10:48:27 by tbolzan-         ###   ########.fr       */
+/*   Updated: 2024/04/01 12:20:51 by tbolzan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,16 @@
 int main()
 {
     ScavTrap tata;
-    
+    ScavTrap tatazinha("tatazinha");
+
+    tata.attack("tatazinha");
+    tata.takeDamage(9);
+    tata.beRepaired(2);
+    tata.takeDamage(20);
+    tatazinha.attack("tata");
+    tatazinha.takeDamage(20);
+    tatazinha.beRepaired(2);
+    tatazinha.takeDamage(20);
+    tatazinha.attack("tata");
 }
 
