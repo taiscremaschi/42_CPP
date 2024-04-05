07ab7f67de4884abb7935de6e0d012fd367ff171
taiscremaschi <taiscremaschi@gmail.com>
2024-04-05 17:51:49 +0100

    starting ex03

---
+++ CPP_04/ex03/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbolzan- <tbolzan-@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/05 17:49:55 by tbolzan-          #+#    #+#             */
+/*   Updated: 2024/04/05 17:50:05 by tbolzan-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int main()
+{
+    IMateriaSource* src = new MateriaSource();
+    src->learnMateria(new Ice());
+    src->learnMateria(new Cure());
+    ICharacter* me = new Character("me");
+    AMateria* tmp;
+    tmp = src->createMateria("ice");
+    me->equip(tmp);
+    tmp = src->createMateria("cure");
+    me->equip(tmp);
+    ICharacter* bob = new Character("bob");
+    me->use(0, *bob);
+    me->use(1, *bob);
+    delete bob;
+    delete me;
+    delete src;
+}
