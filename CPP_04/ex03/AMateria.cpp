873fcd7b9bb4c081f061aea15890a4a69c7489d0
taiscremaschi <taiscremaschi@gmail.com>
2024-04-06 11:46:21 +0100

    progress

--- CPP_04/ex03/AMateria.cpp
+++ CPP_04/ex03/AMateria.cpp
@@ -6,7 +6,24 @@
 /*   By: tbolzan- <tbolzan-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 17:52:36 by tbolzan-          #+#    #+#             */
-/*   Updated: 2024/04/05 17:52:37 by tbolzan-         ###   ########.fr       */
+/*   Updated: 2024/04/06 11:44:53 by tbolzan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "AMateria.hpp"
+
+AMateria::AMateria(){
+    
+}
+
+AMateria::AMateria(std::string const &type){
+    _type = type;
+}
+
+AMateria::~AMateria(){
+    
+}
+
+std::string const &AMateria::getType() const{
+    return _type;
+}
--- CPP_04/ex03/AMateria.hpp
+++ CPP_04/ex03/AMateria.hpp
@@ -6,22 +6,28 @@
 /*   By: tbolzan- <tbolzan-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 17:52:38 by tbolzan-          #+#    #+#             */
-/*   Updated: 2024/04/05 17:53:48 by tbolzan-         ###   ########.fr       */
+/*   Updated: 2024/04/06 11:46:07 by tbolzan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef AMATERIA_HPP
 #define AMATERIA_HPP
 
+#include <iostream>
+
 class AMateria
 {
     protected:
-    
+        std::string _type;
     public:
+        AMateria();
         AMateria(std::string const & type);
-        std::string const & getType() const; //Returns the materia type
-        virtual AMateria* clone() const = 0;
-        virtual void use(ICharacter& target);
+        ~AMateria();
+        AMateria(const AMateria &other); 
+        AMateria &operator=(const AMateria &other);   
+        std::string const &getType() const; 
+        virtual AMateria* clone() const = 0; //falta esse
+        virtual void use(ICharacter& target); //falta esse 
 };
 
 #endif
