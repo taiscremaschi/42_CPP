5fc93f0c079e6d3e747ea077f47dce82b35c3cae
taiscremaschi <taiscremaschi@gmail.com>
2024-06-07 15:08:01 +0100

    start ex00 W

---
+++ CPP_06/ex00/ScalarConverter.cpp
@@ -0,0 +1,21 @@
+
+#include "ScalarConverter.hpp"
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <limits>
+
+
+
+
+
+void ScalarConverter::convert(std::string param){
+ is
+
+
+
+
+}
+
+/*char int double or float*/
---
+++ CPP_06/ex00/ScalarConverter.hpp
@@ -0,0 +1,19 @@
+
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <iostream>
+
+
+class ScalarConverter {
+    private:
+
+    public:
+    static void convert(std::string param);
+
+
+};
+
+
+
+#endif
