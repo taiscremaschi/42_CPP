19b735f675bbae3a977caf794a69118bbe900bd5
taiscremaschi <taiscremaschi@gmail.com>
2024-07-01 11:57:49 +0100

    ex 00 ok

---
+++ CPP_08/ex00/easyfind.hpp
@@ -0,0 +1,25 @@
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iostream>
+#include <string>
+
+class errorParam : public std::exception {
+    virtual const char* what() const throw(){
+        return "The int is not in the container ";
+    }
+    };
+
+template <typename T> 
+int easyfind(T cont, int param)
+{
+    for (size_t i = 0; i < cont.size(); i++)
+    {
+        if(cont[i] == param)
+            return i;
+    }
+    throw (errorParam());
+}
+
+#endif
---
+++ CPP_08/ex00/main.cpp
@@ -0,0 +1,35 @@
+
+#include "easyfind.hpp"
+#include <vector>
+
+int main()
+{
+    std::vector<int> i;
+    i.push_back(100);
+    i.push_back(0);
+    i.push_back(20);
+    i.push_back(-1);
+    i.push_back(200);
+    i.push_back(42);
+    i.push_back(50);
+    i.push_back(2000); 
+
+    try {
+        easyfind(i, 80);
+        std:: cout << "my position in container is " << easyfind(i, 80) << std::endl;   
+    }
+    catch (const std::exception &err) 
+    {
+        std:: cerr << "ERROR: " << err.what() <<std::endl;
+
+    }
+    try {
+        easyfind(i, 42); 
+        std:: cout << "my position in container is " << easyfind(i, 42) << std::endl;   
+    }
+    catch (const std::exception &err) 
+    {
+        std:: cerr << "ERROR: " << err.what() <<std::endl;
+
+    }
+}
