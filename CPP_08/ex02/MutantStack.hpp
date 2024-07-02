b6d664ee61a71e2425dad8df9037f20baad4b5ce
taiscremaschi <taiscremaschi@gmail.com>
2024-07-02 09:51:51 +0100

    start ex02

---
+++ CPP_08/ex02/MutantStack.hpp
@@ -0,0 +1,19 @@
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+
+
+template<typename T>
+class MutantStack : public std::stack<T> {
+    
+
+
+
+};
+
+
+
+#endif
