801403929b1be71adee13d6fcd9786f14bafedf8
taiscremaschi <taiscremaschi@gmail.com>
2024-07-02 17:53:56 +0100

    wp: dataBase

--- CPP_09/ex00/BitcoinExchange.hpp
+++ CPP_09/ex00/BitcoinExchange.hpp
@@ -3,8 +3,34 @@
 
 
 #include <iostream>
-#include <map>
+#include <vector>
 #include <fstream>
+
+
+class Data {
+    private:
+        std::string _data;
+        int _value;
+    public:
+        Data(std::string data, int value);
+        Data(const Data &other);
+        Data &operator=(const Data &other); 
+        ~Data();
+};
+
+class BitcoinExchange {
+    private:
+        std::vector<Data> _dataBase;
+
+    public:
+        BitcoinExchange();
+        BitcoinExchange(const BitcoinExchange &other);
+        BitcoinExchange &operator=(const BitcoinExchange &other); 
+        ~BitcoinExchange();
+
+
+
+};
 
 
 
--- CPP_09/ex00/main.cpp
+++ CPP_09/ex00/main.cpp
@@ -39,7 +39,7 @@ int main(int ac, char **av)
         {
             std::vector<std::string> result = splitInput(buff);
         }
-        
+        //procurar vec 0 na data base. se tiver, vou pegar o numero
 
 
         buff.clear();
