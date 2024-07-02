e49ba767ec0bc1fd6527e685e4a0b53ad4d728d6
taiscremaschi <taiscremaschi@gmail.com>
2024-07-02 17:44:05 +0100

    wp: logic

---
+++ CPP_09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,11 @@
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+
+#include <iostream>
+#include <map>
+#include <fstream>
+
+
+
+#endif
---
+++ CPP_09/ex00/main.cpp
@@ -0,0 +1,53 @@
+#include "BitcoinExchange.hpp"
+#include <vector>
+
+bool checkLine(std::string buff)
+{
+
+
+
+    return true;
+}
+
+std::vector<std::string> splitInput(std::string buff)
+{
+
+
+}
+
+
+int main(int ac, char **av)
+{
+    if(ac != 2){
+        std::cerr << "Error: could not open file." << std::endl;
+        return 1;
+    }
+    std::string filename = av[1];
+
+    //abrir o arq,
+    // ler e guardar a key (data ) e o value (preço)
+    std::ifstream file(filename.c_str());
+    if(!file)
+    {
+        std::cerr << "error in file" << std::endl;
+        return 1;
+    }
+    std::string buff;
+    while(std::getline(file, buff))
+    {
+        if(checkLine(buff))
+        {
+            std::vector<std::string> result = splitInput(buff);
+        }
+        
+
+
+        buff.clear();
+    
+    }
+    
+    
+
+
+
+}
