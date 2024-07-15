f8d6e16bc4a1853e88a0edfe756a03f7bcaba805
taiscremaschi <taiscremaschi@gmail.com>
2024-07-15 19:27:21 +0100

    help

--- CPP_09/ex02/PmergeMe.cpp
+++ CPP_09/ex02/PmergeMe.cpp
@@ -1,9 +1,12 @@
 
 #include "PmergeMe.hpp"
 
-PmergeMe::PmergeMe(std::array<int, 10000> input){
-    
- 
+PmergeMe::PmergeMe(std::array<int, 10000> input, int size){
+    for(size_t i = 0; i < size; i++){
+        _myArray[i] = input[i];
+        _myList.push_back(input[i]);
+    }
+    _size = size;
 }
 
 PmergeMe::PmergeMe(const PmergeMe &other) {
@@ -15,4 +18,46 @@ PmergeMe::~PmergeMe() {}
 PmergeMe &PmergeMe::operator=(const PmergeMe &other){
     (void)  other;
     return *this;
+}
+
+std::array<int, 10000> PmergeMe::organizeArray(){
+    
+
+
+}
+
+std::array<int, 10000> PmergeMe::algArray(std::array<int, 10000> arr){
+    
+
+
+}
+
+
+
+std::list<int> PmergeMe::algList(std::list<int> list){
+
+    int swap = 0;
+
+    if(list.size() < 3){
+        std::list<int>::iterator firstNbr = list.begin();
+        std::list<int>::iterator secondNbr = firstNbr;
+        std::advance(secondNbr, 1);
+        if(*firstNbr > *secondNbr){
+            swap = *firstNbr;
+            *firstNbr = *secondNbr;
+            *secondNbr = swap;
+        }
+        return list;
+    }
+    std::list<int>::iterator it = list.begin();
+    std::advance (it, list.size() / 2);
+    std::list<int> left(list.begin(), it);
+     std::list<int> right(it, list.end());
+    
+    std::list<int>  result = algList(left);
+    algList(right);
+
+
+
+
 }
--- CPP_09/ex02/PmergeMe.hpp
+++ CPP_09/ex02/PmergeMe.hpp
@@ -6,15 +6,22 @@
 #include <fstream>
 #include <stdlib.h>
 #include <array>
+#include <list>
 
 class PmergeMe {
     private:
-
+        std::list<int> _myList;
+        std::array<int, 10000> _myArray;
+        int _size;
     public:
-        PmergeMe(std::array<int, 10000> input);
+        PmergeMe(std::array<int, 10000> input, int size);
         PmergeMe(const PmergeMe &other);
         PmergeMe &operator=(const PmergeMe &other); 
         ~PmergeMe();
+        std::array<int, 10000> algArray(std::array<int, 10000> arr);
+        std::list<int> algList(std::list<int> list);
+        std::array<int, 10000> organizeArray();
+
 
 
 };
